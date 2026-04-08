146e5a799a93c1d62dfaeb9990ee8ca36eb03d7a
Oliver Kmiec <oliverkmiec24@gmail.com>
2026-04-08 17:56:17 -0400

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import Nav from './components/nav';
+import Nav from './components/Nav.jsx';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
